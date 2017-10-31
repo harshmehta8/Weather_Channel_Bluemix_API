f4702a0c69fada1f2a0431b31b7249df9ba32622
harshmehta8 <harsh.cba1408@ict.gnu.ac.in>
2017-10-31 16:07:44 +0530

    Update getthreedaydata.sh

--- getthreedaydata.sh
+++ getthreedaydata.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -o data.json -k https://ef5128a4-86b4-47b8-b14f-a0cd74abf0ef:MwN42ReQZ9@twcservice.mybluemix.net:443/api/weather/v1/geocode/12.97/77.59/forecast/daily/3day.json
+curl -o data.json -k https://5a7a8792-396c-427c-8109-20a12b34c2a9:R8k0wqotp1@twcservice.mybluemix.net:443/api/weather/v1/geocode/12.97/77.59/forecast/daily/3day.json
